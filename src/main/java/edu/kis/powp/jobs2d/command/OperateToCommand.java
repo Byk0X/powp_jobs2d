fcdc859e67618474e90844bc7546fc3aef267414
Byk0X <maciej.lencewicz@gmail.com>
2025-03-20 12:55:36 +0100

    Implements command design pattern

---
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
@@ -0,0 +1,23 @@
+package edu.kis.powp.jobs2d.command;
+
+import edu.kis.powp.jobs2d.drivers.DriverCommand;
+
+public class OperateToCommand implements DriverCommand {
+    private int x;
+    private int y;
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+
+    }
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -0,0 +1,24 @@
+package edu.kis.powp.jobs2d.command;
+
+import edu.kis.powp.jobs2d.drivers.DriverCommand;
+
+public class SetPositionCommand implements DriverCommand {
+
+    private int x;
+    private int y;
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+
+    }
+}
